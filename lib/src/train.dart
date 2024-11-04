7ee1551fff3aecdcbf59bcdb8048a454dbe401bb
Shubham Cognavi <shyam@shyam>
2024-11-04 06:16:32 +0530

    Adding some images

--- lib/src/train.dart
+++ lib/src/train.dart
@@ -24,28 +24,39 @@ class TrainRouteScreen extends StatelessWidget {
               _buildTrainRouteItem(
                 'Charan Paduka',
                 '5 KM',
+                '2 KM',
               ),
               _buildTrainRouteItem(
                 'Ardh Kuwari',
                 '8 KM',
+                '6 KM',
               ),
-              _buildTrainRouteItem(
-                'Sanjichhat',
-                '11 KM',
-              ),
+              _buildTrainRouteItem('Sanjichhat', '11 KM', '9 KM'),
               SizedBox(height: 16.0),
-              Text(
-                'Arrived Ban Ganga',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
-                ),
+              Row(
+                children: [
+                  Text(
+                    'Arrived Ban Ganga',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(width: 8.0),
+                  Text(
+                    'Updated 20 mins ago',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
               ),
               Text(
-                'Updated 20 mins ago',
+                '+2 KM from last stop',
                 style: TextStyle(
-                  fontSize: 14.0,
-                  color: Colors.grey,
+                  fontSize: 12.0,
+                  color: Colors.green,
                 ),
               ),
             ],
@@ -71,10 +82,8 @@ class TrainRouteScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildTrainRouteItem(
-    String milestone,
-    String distance,
-  ) {
+  Widget _buildTrainRouteItem(String milestone,
+      String distanceRemainingLastStop, String distanceRemainingCurrent) {
     return Container(
       padding: const EdgeInsets.all(16.0),
       decoration: BoxDecoration(
@@ -86,6 +95,34 @@ class TrainRouteScreen extends StatelessWidget {
       ),
       child: Row(
         children: [
+          Container(
+            width: 20.0, // Set your desired width
+            height: 20.0, // Set your desired height
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                    'assets/images/oval.png'), // Use your image asset
+                fit: BoxFit.cover, // Adjust the image fit
+              ),
+              borderRadius:
+                  BorderRadius.circular(8.0), // Optional: add some rounding
+            ),
+          ),
+          SizedBox(width: 8.0),
+          Container(
+            width: 50.0, // Set your desired width
+            height: 50.0, // Set your desired height
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                    'assets/images/placeholder.png'), // Use your image asset
+                fit: BoxFit.cover, // Adjust the image fit
+              ),
+              borderRadius:
+                  BorderRadius.circular(8.0), // Optional: add some rounding
+            ),
+          ),
+          SizedBox(width: 8.0),
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -101,9 +138,21 @@ class TrainRouteScreen extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: Text(
-              distance,
-              textAlign: TextAlign.end,
+            child: Row(
+              children: [
+                Text(
+                  distanceRemainingLastStop,
+                  textAlign: TextAlign.end,
+                ),
+                SizedBox(width: 16.0),
+                Text(
+                  "+" + distanceRemainingCurrent,
+                  textAlign: TextAlign.end,
+                  style: TextStyle(
+                    color: Colors.green, // Set text color
+                  ),
+                ),
+              ],
             ),
           ),
         ],
